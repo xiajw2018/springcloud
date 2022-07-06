0d4fb9c6650ddc82c5eb84444e7420607889a001
xiajiawei <xiajiawei@hongxin.com>
2022-07-06 15:43:17 +0800

    spring cloud bus（kafka）消息总线组件添加。已测试

--- client1/src/main/java/com/example/client1/test/TestController.java
+++ client1/src/main/java/com/example/client1/test/TestController.java
@@ -1,10 +1,12 @@
 package com.example.client1.test;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope //只有加了这个注解才能刷新配置
 @RequestMapping("/test")
 @RestController
 public class TestController {
