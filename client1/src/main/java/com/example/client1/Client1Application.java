71d539a7ece475ef8ec8e6d652f8390f00ecff13
xiajiawei <xiajiawei@hongxin.com>
2022-07-08 10:42:21 +0800

    断路器hystrix引入。在服务提供者中引入，可以在发生异常时返回默认值，防止系统阻塞

--- client1/src/main/java/com/example/client1/Client1Application.java
+++ client1/src/main/java/com/example/client1/Client1Application.java
@@ -2,10 +2,13 @@ package com.example.client1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @EnableEurekaClient
+@EnableHystrix
 @SpringBootApplication
 public class Client1Application {
 
--- client1/src/main/java/com/example/client1/test/TestController.java
+++ client1/src/main/java/com/example/client1/test/TestController.java
@@ -1,10 +1,17 @@
 package com.example.client1.test;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCollapser;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RefreshScope //只有加了这个注解才能刷新配置
 @RequestMapping("/test")
@@ -20,5 +27,21 @@ public class TestController {
     @GetMapping("/1")
     public Object test(){
         return String.format("author name :%s,birth : %d",name,birth);
+    }
+
+    /**
+     * 熔断回退
+     */
+    @HystrixCommand(fallbackMethod = "defaultResponse")
+    @GetMapping("/2/{name}")
+    public Object test2(@PathVariable("name")String name)throws Exception{
+        if("0".equals(name)){
+            throw new Exception("123123");
+        }
+        return name;
+    }
+
+    public Object defaultResponse(String name){
+        return "default response test hystrix!";
     }
 }
