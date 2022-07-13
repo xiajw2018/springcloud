85f0f56c7b6af9319ba3dcb4287d6c42a0779cca
xiajiawei <xiajiawei@hongxin.com>
2022-07-13 16:49:54 +0800

    引入consul-bus实现配置动态刷新。

---
+++ client4/consul-bus.md
@@ -0,0 +1,10 @@
+# consul-bus
+
+## maven-repository
+
+        <dependency>
+            <groupId>org.springframework.cloud</groupId>
+            <artifactId>spring-cloud-starter-consul-bus</artifactId>
+        </dependency>
+        
+#### 在需要刷新的类上添加@RefreshScope。当配置更改时，会自动刷新
--- client4/src/main/java/com/example/client4/Client4Application.java
+++ client4/src/main/java/com/example/client4/Client4Application.java
@@ -1,14 +1,29 @@
 package com.example.client4;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+@RefreshScope
 @RestController
+@EnableHystrix
 @SpringBootApplication
 public class Client4Application {
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
 
     @Value("${author.name}")
     private String name;
@@ -24,6 +39,25 @@ public class Client4Application {
     @GetMapping("/config")
     public String testConfig(){
         return String.format("author name : %s \n,birth: %d",name,birth);
+    }
+
+    @GetMapping("/hystrix/{name}")
+    @HystrixCommand(fallbackMethod = "myHystrix")
+    public String testHystrix(@PathVariable("name")String name)throws Exception{
+        if("0".equals(name)){
+            throw new Exception("111");
+        }else{
+            return "success :"+name;
+        }
+    }
+
+    public String myHystrix(String name){
+        return "failed :" + name;
+    }
+
+    @GetMapping("/ribbon")
+    public String testRibbon(){
+        return restTemplate().getForObject("http://client4/config",String.class);
     }
 
     public static void main(String[] args) {
