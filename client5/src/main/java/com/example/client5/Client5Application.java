682719e4fb626c82e67579538fdf6ff8a4603ec3
xiajiawei <xiajiawei@hongxin.com>
2022-09-19 17:24:42 +0800

    spring cloud alibaba 示例添加。nacos作为服务发现，配置中心

---
+++ client5/src/main/java/com/example/client5/Client5Application.java
@@ -0,0 +1,79 @@
+package com.example.client5;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RefreshScope
+@RestController
+@EnableHystrix
+@EnableDiscoveryClient
+@SpringBootApplication
+public class Client5Application {
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+
+    @Value("${author.name}")
+    private String name;
+
+    @Value("${author.birth}")
+    private Integer birth;
+
+    @GetMapping("/")
+    public String home(){
+        return "welcome to consul.consul can be used insteadof eureka.bus";
+    }
+
+    @GetMapping("/config")
+    public String testConfig(){
+        return String.format("author name : %s \n,birth: %d",name,birth);
+    }
+
+    @GetMapping("/sleep/{millis}")
+    public String testSleep(@PathVariable("millis")Long millis){
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return "sleep finished!";
+    }
+
+    @GetMapping("/hystrix/{name}")
+    @HystrixCommand(fallbackMethod = "myHystrix")
+    public String testHystrix(@PathVariable("name")String name)throws Exception{
+        if("0".equals(name)){
+            throw new Exception("111");
+        }else{
+            return "success :"+name;
+        }
+    }
+
+    public String myHystrix(String name){
+        return "failed :" + name;
+    }
+
+//    @GetMapping("/ribbon")
+//    public String testRibbon(){
+//        return restTemplate().getForObject("http://client4/config",String.class);
+//    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Client5Application.class, args);
+    }
+
+}
